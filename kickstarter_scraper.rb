f3c1c539c6d236ff43acdf23154dffd976f0c128
Priscilla H <priscillamhuynh@gmail.com>
2018-07-12 13:49:03 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,4 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 #locations: project.css("span.location-name").text
+#percent funded: project.css("ul.project-stats li.first.funded strong").text
