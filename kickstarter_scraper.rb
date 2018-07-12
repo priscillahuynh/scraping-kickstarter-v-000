359306d4a10dc9f2e36660249cb4bb63ffa60f58
Priscilla H <priscillamhuynh@gmail.com>
2018-07-12 13:33:12 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,9 @@
 require 'nokogiri'
+require 'pry'
 # require libraries/modules here
 
 def create_project_hash
   html=File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  binder.pry
 end
